86e81c56156b2ef0bd281a912e16c973a75bf97f
Hubert <hpan3@gatech.edu>
2017-04-22 15:08:17 -0400

    close file, fix tmhmm detection and only recoordinate positions when gene start is greater than feature start

--- convertCoord.py
+++ convertCoord.py
@@ -8,6 +8,8 @@ def generateNewLineEntry(lineArray):
     return "\t".join(map(str, lineArray))
 
 def convertStartAndEndPoints(line):
+    global filename
+    #print "{}".format(filename[1])
     #strip out leading and trailing whitespace
     line = line.strip()
 
@@ -22,11 +24,18 @@ def convertStartAndEndPoints(line):
                 geneStart = int(rem.group(3))
                 featureStart_gene = int(lineSplitByTabs[3])
                 featureEnd_gene = int(lineSplitByTabs[4])
-                if(fileName[1].startswith("TMHMM")):
+                #do we need to convert AA coordinates to DNA coordinates
+                if((filename[1].find("TMHMM.gff") != -1) or (filename[1].find("InterProScan.gff") != -1)):
                     featureStart_gene = 3 * featureStart_gene
                     featureEnd_gene = 3 * featureEnd_gene
-                featureStart_global = geneStart + featureStart_gene
-                featureEnd_global = geneStart + featureEnd_gene;
+
+                #Do we need to convert gene based coordinates into genome based coordinates
+                if(geneStart > featureEnd_gene):
+                    featureStart_global = geneStart + featureStart_gene
+                    featureEnd_global = geneStart + featureEnd_gene;
+                else:
+                    featureStart_global = featureStart_gene
+                    featureEnd_global = featureEnd_gene;
                 newSequenceName = "{}:{}".format(rem.group(1), rem.group(2));
                 newLineSplitByTabs = [newSequenceName, lineSplitByTabs[1], lineSplitByTabs[2], featureStart_global, featureEnd_global, lineSplitByTabs[5], lineSplitByTabs[6], lineSplitByTabs[7], lineSplitByTabs[8]]
                 print generateNewLineEntry(newLineSplitByTabs)
@@ -35,7 +44,7 @@ def convertStartAndEndPoints(line):
                 pass
 
         else:
-            print "#9:{0}".format(line)
+            print "{0}".format(line)
     else:
         print "#c{0}".format(line)
 
@@ -43,5 +52,6 @@ def main(fileName):
     targetFile = open(fileName[1])
     for line in targetFile:
         convertStartAndEndPoints(line)
+    targetFile.close()
 
 main(filename)
